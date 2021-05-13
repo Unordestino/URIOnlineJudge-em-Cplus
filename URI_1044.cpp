03ff75eb5ff010c8fed940213be7fcde89b666ab
Davisilveira <80595329+Unordestino@users.noreply.github.com>
2021-05-13 18:37:53 -0300

    Add files via upload

---
+++ URI_1044.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using namespace std;
+
+int main ()
+{
+    int A, B, aux;
+
+    cin >> A >> B;
+
+    if (A < B){
+        aux = A;
+        A = B;
+        B = aux;
+    }
+    if(A%B == 0){
+    cout << "Sao Multiplos" << endl;
+    }
+    else{
+    cout << "Nao sao Multiplos" << endl;
+    }
+
+    return 0;
+}
---
+++ URI_1045.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+
+using namespace std;
+
+int main ()
+{
+    double A, B, C;
+
+    cin >> A >> B >> C;
+
+    if (A >= (B+C) || B >= (A+C) || C >= (A+B) ){
+        cout << "NAO FORMA TRIANGULO" << endl;
+    }
+    else if( (A*A) == (B*B + C*C) || B*B == (A*A + C*C) || C*C == (A*A + B*B) ){
+        cout << "TRIANGULO RETANGULO" << endl;
+    }
+    else if(A*A > (B*B + C*C) || B*B >  (A*A + C*C) || C*C > (A*A + B*B) ){
+        cout << "TRIANGULO OBTUSANGULO" << endl;
+    }
+    else if(A*A < (B*B + C*C) || B*B < (A*A + C*C) || C*C < (A*A + B*B) ){
+        cout << "TRIANGULO ACUTANGULO" << endl;
+    }
+    if(A == B && B == C){
+        cout << "TRIANGULO EQUILATERO" << endl;
+    }
+    if( (A == B && A != C) || (A == C && A != B) || (B == C && B != A)  )
+        {
+        cout << "TRIANGULO ISOSCELES" << endl;
+        }
+
+    return 0;
+}
